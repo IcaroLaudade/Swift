900c834c3b8e1b4e53ab9f286b64ad97be00be0a
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-23 15:58:02 -0300

    Aprendizado Swift

---
+++ Vetor/Vetor/Numeros.swift
@@ -0,0 +1,23 @@
+﻿public class Numeros {
+    var valores=[Int]()
+    init(valores:Array<Int>){
+        valores.sort {
+            return $0 < $1         
+        }
+        self.valores=valores   
+    }
+    func localizarMaior(){
+
+        print(valores.last)
+    }
+    func localizarMenor(){
+        print(valores.first)
+    }
+    func calcularMedia(){
+        var soma=0
+        for valor in valores{
+        soma+=valor
+        }
+        print(soma)
+    }
+}
---
+++ Vetor/Vetor/Program.swift
@@ -0,0 +1,11 @@
+﻿import System.Collections.Generic
+import System.Linq
+import System.Text
+import System.Threading.Tasks
+
+let ar:Array<Int> = [5,99,99,98,1,0]
+var numeros=Numeros(valores:ar)
+numeros.calcularMedia()
+numeros.localizarMaior()
+numeros.localizarMenor()
+Console.ReadKey()
