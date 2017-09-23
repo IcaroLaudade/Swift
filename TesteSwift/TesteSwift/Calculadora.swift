b55e326825fe11685e520f611bb7b032558e9c3b
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-23 16:22:38 -0300

    Calculadora

---
+++ TesteSwift/TesteSwift/Calculadora.swift
@@ -0,0 +1,21 @@
+﻿public class Calculadora {
+	func operacao(a:Double,b:Double,operador:String)->Double?{
+		switch(operador){
+			case "+":
+				return a+b
+			case "-":
+				return a-b
+			case "*":
+				return a*b
+			case "/":
+				if(b==0){
+					return nil
+				}else{
+					return a/b
+				}
+			default:
+				return 0
+		}
+	
+	}
+}
---
+++ TesteSwift/TesteSwift/Program.swift
@@ -0,0 +1,20 @@
+﻿import System.Collections.Generic
+import System.Linq
+import System.Text
+import System.Threading.Tasks
+
+print("Hello World!")
+var qtdVar=10
+qtdVar=11
+let qtd = 11
+var dez=10
+func multiplicar(a:Int,b:Int)->Int{
+	return a*b
+}
+var c= multiplicar(a: 15, b:  20)
+print( c)
+var inte:Double?
+var calculadora = Calculadora()
+inte =calculadora.operacao(a:10.0,b:0.0,operador: "/")
+print(inte)
+Console.ReadKey()
