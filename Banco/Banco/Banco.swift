f0b9503cd992d0499d8fad49a2f0731be709e7fc
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-23 17:48:09 -0300

    Exercício Banco- Swift

---
+++ Banco/Banco/Banco.swift
@@ -0,0 +1,23 @@
+﻿public class Banco {
+    private var numeroConta=123456
+    private var saldo:Double?
+    func getExtrato() -> String?{
+        var num=Double(saldo!)
+        return numeroConta+" "+"\(num)"
+    }
+  public func sacar(valorASacar:Double){
+        if valorASacar<saldo{
+            saldo-=valorASacar
+        }
+    }
+   public func depositar(valorADepositar:Double){
+        if valorADepositar>0.00{
+            if saldo==nil{
+                saldo=0
+            }
+            saldo+=valorADepositar
+        }
+    }
+
+
+}
---
+++ Banco/Banco/Program.swift
@@ -0,0 +1,8 @@
+﻿import System.Collections.Generic
+import System.Linq
+import System.Text
+import System.Threading.Tasks
+var banco=Banco()
+banco.depositar(valorADepositar: 1000)
+print(banco.getExtrato())
+Console.ReadKey()
